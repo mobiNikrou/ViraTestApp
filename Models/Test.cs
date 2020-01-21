e8effa4aa94b7879c8fbb6f3739b382a12ce46db
mobiNikrou <mobin.nikrou@gmail.com>
2020-01-21 08:45:49 +0330

    Add Test Model

---
+++ Models/Test.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Web;
+
+namespace ViraTestApp.Models
+{
+    public class Test
+        
+    {
+        [Display(Name = "Project Name")]
+        public ProjectNames ProjectName { get; set; }
+        [Display(Name = "Application Name")]
+        public ApplicatioNames ApplicatioName  { get; set; }
+        [Display(Name = "Test Type")]
+        public TestTypes TestType { get; set; }
+    }
+    public class TestingResultModel
+
+    {
+        [Display(Name = "TestCheck1")]
+        public string TestCheck1 { get; set; }
+        [Display(Name = "TestCheck2")]
+        public string TestCheck2 { get; set; }
+        [Display(Name = "TestCheck3")]
+        public string TestCheck3 { get; set; }
+    }
+    public enum ProjectNames
+    {
+        lifeAlborzInsurance = 1,
+        AlborzInsurance = 2
+    }
+    public enum ApplicatioNames
+    {
+        AlborzContactManagingDirector = 1,
+    }
+    public enum TestTypes
+    {
+        AcceptanceTest = 1,
+        OwaspTest = 2,
+    }
+
+}
