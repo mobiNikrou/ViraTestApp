993cbfbd1a195d3702b027fb4076421f68f8451f
mobiNikrou <mobin.nikrou@gmail.com>
2020-01-21 10:21:51 +0330

    Add Test Form In Index.cshtml & HandelForm Action

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -17,11 +17,49 @@ namespace ViraTestApp.Controllers
         {
             _logger = logger;
         }
-
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
+        
+        [HttpPost]
+        public ActionResult HandelForm(Test ApplicatioNames, Test TestTypes, Test ProjectNames)
+        {
+            string ProjectName = ProjectNames.ProjectName.ToString();
+            string TestType = TestTypes.TestType.ToString();
+            string ApplicatioName = ApplicatioNames.ApplicatioName.ToString();
+            // App1TestCases Testobj = new App1TestCases();
+            switch (ProjectName)
+            {
+                case "lifeAlborzInsurance":
+                {
+                    switch (ApplicatioName)
+                    {
+                        case "AlborzContactManagingDirector":
+                        {
+                            switch (TestType)
+                            {
+                                case "AcceptanceTest":
+                                    // string[] testrepo = Testobj.TestAction1();
+                                    // IList<TestingResultModel> testindexlist = new List<TestingResultModel>();
+                                    // testindexlist.Add(new TestingResultModel() { TestCheck1 = testrepo[0], TestCheck2 = testrepo[1], TestCheck3 = testrepo[2] });
+
+                                    // ViewData["testindexlist"] = testindexlist;
+                                    // return View("TestResult");
+                                    break;
+  
+                            }
+                        }
+                            break;
+                    }
+                    break;
+                }
+            }
+            
+            return View("Index");
+        }
+
 
         public IActionResult Privacy()
         {
